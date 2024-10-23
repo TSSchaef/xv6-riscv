48ece827e903179f778893d92fb29270055609f7
Tyler Schaefer <tschaeferstorm@gmail.com>
2024-10-23 14:20:27 -0500

    Added template for tests

---
+++ user/schedtestc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void test1(){
+  printf("Test 1: PASSED");
+}
+
+void test2(){
+  printf("Test 2: PASSED");
+}
+
+int main(int argc, char *argv[]){
+  test1();
+  test2();
+  return 0;
+}
